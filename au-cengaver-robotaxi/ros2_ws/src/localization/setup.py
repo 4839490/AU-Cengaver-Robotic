40c07caaac5cd5e31c3953aa689cbebeb7085036
Ahmet Salih Uluışık <159609441+4839490@users.noreply.github.com>
2026-05-13 13:30:23 +0300

    Update setup.py

--- au-cengaver-robotaxi/ros2_ws/src/localization/setup.py
+++ au-cengaver-robotaxi/ros2_ws/src/localization/setup.py
@@ -9,13 +9,27 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.py')),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml'],
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py'),
+        ),
+        (
+            os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml'),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'numpy',
+    ],
     zip_safe=True,
     maintainer='Ahmet Salih Uluşık',
     maintainer_email='cengaver@example.com',
